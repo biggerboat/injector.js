9498cc01a37b7577db0c00658a387ee551895269
Paul Tondeur <paul@paultondeur.nl>
2013-05-11 16:32:02 +0200

    RequireJS shim configured so all dependencies will be loaded in the correct order and recompiled injector.min.js with latest changes (now supports named injection without type specification)

--- src/public_html/assets/js/main.js
+++ src/public_html/assets/js/main.js
@@ -1,25 +1,25 @@
-//requirejs.config({
-//    paths: {
-//        namespaces: 'namespaces',
-//        Injector: 'Injector',
-//        InjectorUtils: 'InjectorUtils',
-//        InjectionMapping: 'InjectionMapping'
-//    },
-//
-//    shim: {
-//        "Injector": {
-//            "deps": ["namespaces"]
-//        },
-//
-//        "InjectorUtils": {
-//            "deps": ["Injector"]
-//        },
-//
-//        "InjectionMapping": {
-//            "deps": ["Injector"]
-//        }
-//    }
-//});
+requirejs.config({
+	paths: {
+		namespaces: 'namespaces',
+		Injector: 'Injector',
+		InjectorUtils: 'InjectorUtils',
+		InjectionMapping: 'InjectionMapping'
+	},
+
+	shim: {
+		"Injector": {
+			"deps": ["namespaces"]
+		},
+
+		"InjectorUtils": {
+			"deps": ["Injector"]
+		},
+
+		"InjectionMapping": {
+			"deps": ["Injector"]
+		}
+	}
+});
 
 require(["namespaces",
 		 "Injector",
