e2bfdf6385c1d56aa00991cdf8c5844674753e80
Paul Tondeur <paul@paultondeur.nl>
2013-10-05 18:04:54 +0200

    Implemented grunt-bump releasing

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,22 @@ module.exports = function (grunt) {
 		watch: {
 			files: ['public/js/*.js'],
 			tasks: ['concat:dist','uglify:dist', 'copy:injector']
+		},
+
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				updateConfigs: ["pkg","banner"],
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json', 'bower.json', '<%= pkg.name %>.min.js', '<%= pkg.name %>.js'], // '-a' for all files
+				createTag: true,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: true,
+				pushTo: 'origin master',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+			}
 		}
 	});
 
@@ -44,6 +60,12 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-bump');
 
 	grunt.registerTask('default', ['concat','uglify','copy']);
+
+	grunt.registerTask('release', ['default','bump-commit']);
+	grunt.registerTask('release:patch', ['bump-only:patch','release']);
+	grunt.registerTask('release:minor', ['bump-only:minor','release']);
+	grunt.registerTask('release:major', ['bump-only:major','release']);
 };
